de708a27b2aad5678286aa03886cea18e8ecbd4a
gregory goren <grggoren@gmail.com>
2019-06-08 18:48:49 +0300

    testing

--- vocabulary/Lang.py
+++ vocabulary/Lang.py
@@ -23,7 +23,7 @@ class LanguageHelper():
                 if rnum%1000==0:
                     logger.info("in index ",rnum)
                 sentence = row["proc_sentence"]
-                print(sentence)
+                print(rnum,sentence)
                 tokens = sentence.split()
                 for token in tokens:
                     word = token.lower()
