c60f72e2e60427b08b71965a30471e844d739118
gregory goren <grggoren@gmail.com>
2019-06-13 18:04:35 +0300

    testing

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ if __name__=="__main__":
     logger.info("running %s" % ' '.join(sys.argv))
     w2v_model_file_path = sys.argv[1]
     data_set_file_path = sys.argv[2]
-    w2v_model = gensim.models.KeyedVectors.load_word2vec_format(w2v_model_file_path,limit = 700000  ,binary=True)
+    w2v_model = gensim.models.KeyedVectors.load_word2vec_format(w2v_model_file_path,binary=True)
     rows,cols = w2v_model.wv.vectors.shape
     SOS_idx = rows
     EOS_idx = rows+1
     PAD_idx = rows+2
-    df = pd.read_csv(data_set_file_path,delimiter=",")
+    df = pd.read_csv(data_set_file_path,delimiter=",",nrows=5000)
     n_layers = 1
     hidden_size = 200
     lr = 0.01
