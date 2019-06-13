dd6e8c90fd57c34538619580e0d1a8776e3808c1
gregory goren <grggoren@gmail.com>
2019-06-13 16:07:12 +0300

    testing

--- w2v/reduce_data_set.py
+++ w2v/reduce_data_set.py
@@ -1,15 +1,36 @@
 import gensim
 import pandas as pd
 import sys
+import os
+import logging
+from multiprocessing.pool import Pool
+from functools import partial
+def f(sentence,model):
+    program = os.path.basename(sys.argv[0])
+    logger = logging.getLogger(program)
+
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
+    logging.root.setLevel(level=logging.INFO)
+    logger.info("running %s" % ' '.join(sys.argv))
+
 if __name__=="__main__":
     data_path = sys.argv[1]
     model_path = sys.argv[2]
+    program = os.path.basename(sys.argv[0])
+    logger = logging.getLogger(program)
+
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
+    logging.root.setLevel(level=logging.INFO)
+    logger.info("running %s" % ' '.join(sys.argv))
     df = pd.read_csv(data_path,delimiter=",",chunksize=100000)
     model = gensim.models.KeyedVectors.load_word2vec_format(model_path,limit = 700000  ,binary=True)
-    new_df = pd.DataFrame(columns=['article_uuid', 'sentence', 'proc_sentence', 'proc_len'])
+    new_df = pd.DataFrame(columns=['article_uuid', 'proc_sentence', 'proc_len'])
+    rnum=1
     for chunk in df:
         for index,row in chunk.iterrows():
+            logger.info("Processing row:"+str(rnum))
             proc_sentence = row["proc_sentence"]
+            del row['sentence']
             sentence = str(proc_sentence)
             write = True
             for token in sentence.split():
@@ -18,5 +39,6 @@ if __name__=="__main__":
                     break
             if write:
                 new_df.append(row)
-    new_df.to_csv("reduced_wiki_sentences.csv")
+            rnum+=1
+    new_df.to_csv("reduced_wiki_sentences1.csv")
 
