afb2414286b93823cd3b0f90d4c0e423d77c6599
gregory goren <grggoren@gmail.com>
2019-06-16 13:28:41 +0300

    L

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ if __name__=="__main__":
     n_layers = 1
     hidden_size = 200
     lr = 0.01
-    batch_size = 5
+    batch_size = 4
     epochs = 5
     train_model(lr,batch_size,epochs,hidden_size,n_layers,w2v_model,SOS_idx,EOS_idx,PAD_idx,df,logger)
 
