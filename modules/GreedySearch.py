62d80705cb714c6a560692af729d624637635a90
gregory goren <grggoren@gmail.com>
2019-06-25 14:55:42 +0300

    L

--- modules/GreedySearch.py
+++ modules/GreedySearch.py
@@ -12,9 +12,9 @@ def greedy_generation(model, x, lengths,max_generation_len,device):
         decoder_output, decoder_hidden = model.decoder(input, (decoder_hidden_h, decoder_hidden_c))
         decoder_hidden_h, decoder_hidden_c = decoder_hidden
         # h: (vocab_size)
-        h = model.W(decoder_output.squeeze(1))
+        h = model.W(decoder_output.squeeze(1)).squeeze(0)
         # y = softmax(h)
-        y = log_softmax(h,dim=1).squeeze(0)
+        y = log_softmax(h,dim=0)
         _, current_y = torch.max(y, dim=0)
         current_y = current_y.item()
         result.append(current_y)
