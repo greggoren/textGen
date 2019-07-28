adf3b5b5dfd001565ec611f5e0fea3e06edbec2b
gregory goren <grggoren@gmail.com>
2019-07-28 12:05:28 +0300

    L

--- translation_dataset_preprocess/transform_ds.py
+++ translation_dataset_preprocess/transform_ds.py
@@ -5,8 +5,12 @@ if not os.path.exists(new_input_dir):
     os.makedirs(new_input_dir)
 for file in os.listdir(input_dir):
     new_file_name = new_input_dir+file
+    flag = True
     with open(input_dir+file) as original_file:
         with open(new_file_name,'w') as new_file:
             for line in original_file:
+                if flag:
+                    flag=False
+                    continue
                 new_line = "\t".join(line.split(",")[1:]).rstrip()+"\n"
                 new_file.write(new_line)
