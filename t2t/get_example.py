927ab834a5a40ce5c1958f13f226af808da5017c
gregory goren <grggoren@gmail.com>
2019-07-30 15:33:46 +0300

    L

--- t2t/get_example.py
+++ t2t/get_example.py
@@ -1,29 +1,24 @@
 import tensorflow as tf
 
-filenames = "~/t2t_data/translation_query-dev-00000-of-00001"
-with tf.Graph().as_default():
-    dataset = tf.data.Dataset.from_tensor_slices(filenames)
+def extract_fn(data_record):
+    features = {
+        # Extract features using the keys set during creation
+        'inputs': tf.FixedLenFeature([], tf.string),
+        'targets': tf.FixedLenFeature([], tf.string),
+    }
+    sample = tf.parse_single_example(data_record, features)
+    return sample
 
-def _load_records(filename):
-  return tf.data.TFRecordDataset(
-      filename,
-      compression_type=tf.constant("GZIP") if False else None,
-      buffer_size=16 * 1000 * 1000)
-
-dataset = dataset.flat_map(_load_records)
-
-def _parse_example(ex_ser):
-  return tf.parse_single_example(ex_ser, False)
-
-if False:
-  dataset = dataset.map(_parse_example, num_parallel_calls=32)
-dataset = dataset.prefetch(100)
-record_it = dataset.make_one_shot_iterator().get_next()
+# Initialize all tfrecord paths
+dataset = tf.data.TFRecordDataset(["~/t2t_data/translation_query-dev-00000-of-00001"])
+dataset = dataset.map(extract_fn)
+iterator = dataset.make_one_shot_iterator()
+next_element = iterator.get_next()
 
 with tf.Session() as sess:
-  while True:
     try:
-      ex = sess.run(record_it)
-      print(ex)
-    except tf.errors.OutOfRangeError:
-      break
+        while True:
+            data_record = sess.run(next_element)
+            print(data_record)
+    except:
+        pass
