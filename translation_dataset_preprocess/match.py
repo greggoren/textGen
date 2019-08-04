47eb6bc0cbf6677dfe5ee275205d64d11bd4b46d
gregory goren <grggoren@gmail.com>
2019-08-04 11:12:39 +0300

    L

--- translation_dataset_preprocess/match.py
+++ translation_dataset_preprocess/match.py
@@ -28,6 +28,7 @@ def match(input_dir,target_dir,new_target_dir):
             rows[row]["query"]=file
             rows[row]["input_sentence"]=sentence
             rows[row]["target_sentence"]=target_sentence
+            row+=1
         df = pd.DataFrame.from_dict(rows)
         df.to_csv(new_target_dir+file+".csv")
 
