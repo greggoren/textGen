5adcfa2a86b464869d04a1fd49ed58aeefa5d4e6
gregory goren <grggoren@gmail.com>
2019-08-08 14:49:58 +0300

    L

--- t2t/utils.py
+++ t2t/utils.py
@@ -4,12 +4,13 @@ def run_command(command):
                          stdout=subprocess.PIPE,
                          stderr=subprocess.STDOUT,
                          shell=True)
+
     return iter(p.stdout.readline, b'')
 
 def run_bash_command(command):
     p = subprocess.Popen(command,
                          stdout=subprocess.PIPE,
                          stderr=subprocess.STDOUT, shell=True)
-
+    p.wait()
     out, err = p.communicate()
     return out
