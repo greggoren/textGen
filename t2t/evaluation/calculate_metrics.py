49689c8a796619de43c5f5098425cc32fc5a49fb
gregory goren <grggoren@gmail.com>
2019-08-08 14:25:31 +0300

    L

--- t2t/evaluation/calculate_metrics.py
+++ t2t/evaluation/calculate_metrics.py
@@ -2,13 +2,34 @@ import os
 from t2t.utils import run_bash_command
 import time
 from optparse import OptionParser
-
-def run_bleu(translation,reference,script):
+from multiprocessing import Pool
+from functools import partial
+def run_bleu(reference,script,translation):
     out = run_bash_command(script+" --translation="+translation+" --reference="+reference)
-    for line in out.split("\n"):
+    for line in str(out).split("\n"):
         if "BLEU_uncased" in line:
             score = float(line.split()[2].rstrip())
-            return score
+            return score,translation
+
+
+
+
+def calculate_bleu_multiprocess(translations_dir,reference_file,bleu_script_bin,write_flag,processes):
+    files = [file for file in os.listdir(translations_dir)]
+    files = sorted(files)
+    bleus = []
+    if write_flag:
+        ts = time.time()
+        bleu_results_file = open("BLEU_RESULTS_"+str(ts),
+                                 'w')
+    func = partial(run_bleu,reference_file,bleu_script_bin)
+    with Pool(processes=processes) as pool:
+        results = pool.map(func,files)
+        if write_flag:
+            for result in results:
+                bleu_results_file.write(result[1]+"\t"+result[0]+"\n")
+            bleu_results_file.close()
+    return bleus
 
 
 def calculate_bleu(translations_dir,reference_file,bleu_script_bin,write_flag):
@@ -21,7 +42,7 @@ def calculate_bleu(translations_dir,reference_file,bleu_script_bin,write_flag):
                                  'w')
     for file in files:
         translation_file = translations_dir+file
-        score = run_bleu(translation_file,reference_file,bleu_script_bin)
+        score,_ = run_bleu(reference_file,bleu_script_bin,translation_file)
         bleus.append(score)
         if write_flag:
             bleu_results_file.write(file+"\t"+str(score)+"\n")
@@ -41,6 +62,7 @@ if __name__=="__main__":
     parser.add_option("-s", "--bleu_script", dest="bleu_script")
     parser.add_option("-w", "--write", dest="write",action="store_true")
     parser.add_option("-n", "--no_write", dest="write",action="store_false")
+    parser.add_option("-k", "--multiproccess", dest="multi",default='0')
     (options, args) = parser.parse_args()
     metric = options.metric
     translation_dir = options.translations_dir
@@ -48,4 +70,7 @@ if __name__=="__main__":
     reference_file = options.reference
     write_flag = options.write
     if metric.lower()=="bleu":
-        calculate_bleu(translation_dir,reference_file,bleu_script_path,write_flag)
+        if options.multi=='0':
+            calculate_bleu(translation_dir,reference_file,bleu_script_path,write_flag)
+        else:
+            calculate_bleu_multiprocess(translation_dir,reference_file,bleu_script_path,write_flag,int(options.multi))
