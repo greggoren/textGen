05bfa7fb21178c33a59ecc20a2c22e563fcb60b4
gregory goren <grggoren@gmail.com>
2019-08-14 11:41:01 +0300

    L

---
+++ t2t/evaluation/graphs.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+import os
+
+def read_file(fname):
+    results = {}
+    steps = []
+    with open(fname) as file:
+        for line in file:
+            step = int(line.split("\t")[0].split("_")[1].split(".")[0])
+            steps.append(step)
+            results[step] = float(line.split("\t")[1].rstrip())
+    y_axis = [i[1] for i in sorted(results.items(),key=lambda item:item[0])]
+    return y_axis,sorted(steps)
+
+def get_ylabel(fname):
+    if fname.__contains__("BLEU"):
+        return "BLEU"
+    elif fname.__contains__("COVERAGE"):
+        return "Query Coverage"
+    elif fname.__contains__("ACCURACY"):
+        return "Accuracy"
+    elif fname.__contains__("SIMILARITY"):
+        return "Similarity"
+
+def generate_graphs_results_dir(results_dir):
+    for file in os.listdir(results_dir):
+        fname = results_dir+file
+        y,x = read_file(fname)
+        plot_metric(y,x,"_".join(file.split("_")[:-1]),get_ylabel(file),"Steps",None,None)
+
+
+def plot_metric(y,x,fname,y_label,x_label,legends=None,colors=None):
+    params = {'legend.fontsize': 'x-large',
+              'figure.figsize': (13, 8),
+              'axes.labelsize': 'x-large',
+              'axes.titlesize': 'x-large',
+              'xtick.labelsize': 'small',
+              'ytick.labelsize': 'x-large',
+              'font.family': 'serif'}
+
+
+    plt.rcParams.update(params)
+    plt.figure()
+    if legends is not None:
+        for j,y_m in enumerate(y):
+            plt.plot(x, y_m, color=colors[j], linewidth=5,markersize=5, mew=1,label=legends[j])
+    else:
+
+        plt.plot(x, y, color='b', linewidth=5,markersize=5, mew=1)
+
+    # plt.axis([1, len(x), 1.1 * np.min(x), 2 * np.max(x)])
+    plt.xticks(x,fontsize=15)
+
+    plt.yticks(fontsize=25)
+    plt.ylabel(y_label, fontsize=30)
+    plt.xlabel(x_label, fontsize=30)
+    plt.legend(loc="best")
+    plt.savefig(fname+".png")
+    plt.clf()
+
+generate_graphs_results_dir("results/")
---
+++ translation_dataset_preprocess/prepare_ds.py
@@ -0,0 +1,21 @@
+import os
+
+def make_new_file(old_file,new_file):
+    with open(new_file,"w") as nfile:
+        with open(old_file) as ofile:
+            for line in ofile:
+                input_sequence = line.split(",")[2]
+                query = line.split(",")[1]
+                new_input_sequence = input_sequence+" "+query
+                new_row = ",".join([line.split(",")[0],line.split(",")[1],new_input_sequence,line.split(",")[3]])+"\n"
+                nfile.write(new_row)
+
+def prepare_query_based_ds(original_dir,new_dir):
+    if not os.path.exists(new_dir):
+        os.makedirs(new_dir)
+    for file in os.listdir(original_dir):
+        make_new_file(original_dir+file,new_dir+file)
+
+
+prepare_query_based_ds("translations_new_ds/","translations_new_ds_with_query/")
+
