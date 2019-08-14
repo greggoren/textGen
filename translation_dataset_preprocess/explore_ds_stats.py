933022d0be666ec2c715c0d8007ba2ff01e9f8f3
gregory goren <grggoren@gmail.com>
2019-08-14 12:31:16 +0300

    L

--- translation_dataset_preprocess/explore_ds_stats.py
+++ translation_dataset_preprocess/explore_ds_stats.py
@@ -9,4 +9,16 @@ def number_of_unique_targets(ds_dir):
         unique_num_file.write(file+" "+str(count_target_values)+"\n")
     unique_num_file.close()
 
+
+def variance_of_target_appearance(ds_dir):
+    variance_file = open("variance_target_exploration_stats.txt","w")
+    for file in os.listdir(ds_dir):
+        df = pd.read_csv(ds_dir+file,header=0,delimiter=",")
+        variance_target_values = df.target_sentence.value_counts().var()
+        variance_file.write(file+" "+str(variance_target_values)+"\n")
+    variance_file.close()
+
+
+
 number_of_unique_targets('translations_new_ds_with_query/')
+variance_of_target_appearance('translations_new_ds_with_query/')
