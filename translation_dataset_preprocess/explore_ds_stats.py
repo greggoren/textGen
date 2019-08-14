60e0743a8be943005176f29e8ffa300e78a67b92
gregory goren <grggoren@gmail.com>
2019-08-14 12:01:44 +0300

    L

---
+++ translation_dataset_preprocess/explore_ds_stats.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+
+def number_of_unique_targets(ds_dir):
+    unique_num_file = open("unique_target_exploration_stats.txt","w")
+    for file in os.listdir(ds_dir):
+        df = pd.read_csv(ds_dir+file,header=0,delimiter=",")
+        count_target_values = df.target_sentence.nunique()
+        unique_num_file.write(file+" "+str(count_target_values)+"\n")
+    unique_num_file.close()
+
+number_of_unique_targets('translations_new_ds_with_query/')
