c3f6eb5c8ae055c9e73d62d7e3371d2056222295
gregory goren <grggoren@gmail.com>
2019-09-02 16:04:47 +0300

    L

--- summarization/new_input_pool.py
+++ summarization/new_input_pool.py
@@ -105,7 +105,7 @@ def get_centroid_of_cluster(df):
 def cosine_similarity(v1,v2):
     sumxx, sumxy, sumyy = 0, 0, 0
     for i in range(len(v1)):
-        x = v1[i]; y = v2[i]
+        x = float(v1[i]); y = float(v2[i])
         sumxx += x*x
         sumyy += y*y
         sumxy += x*y
