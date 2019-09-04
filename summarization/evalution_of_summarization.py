dc8e53dd38883fcfa22506d85fbe13c723d944a4
gregory goren <grggoren@gmail.com>
2019-09-04 14:33:30 +0300

    L

--- summarization/evalution_of_summarization.py
+++ summarization/evalution_of_summarization.py
@@ -26,6 +26,18 @@ def query_coverage(query,output):
         denominator+=1
     return numerator/denominator
 
+def query_coverage_recall(query,output,input):
+    numerator=0
+    denominator=0
+    for q in query.split():
+        if q in output and q in input:
+            numerator+=1
+        if q in input:
+            denominator+=1
+    return numerator/denominator
+
+
+
 def is_query_included(query,output):
     for q in query.split():
         if q in output:
@@ -58,6 +70,10 @@ def get_sentence_centroid(sentence,model):
     if sum_vector is None:
         return None
     return sum_vector / denom
+
+
+
+
 
 def similarity_to_source(source,output,model,segments):
     for segment in segments:
@@ -94,6 +110,19 @@ def get_query_coverage_on_all_output(qfname,out_fname):
         query = query_lines[row]
         numerator+=query_coverage(query,out_line)
     return numerator/(row+1)
+
+def get_query_coverage_recall_on_all_output(qfname,out_fname,in_fname):
+    out_lines = read_file(out_fname)
+    query_lines = read_file(qfname)
+    in_lines = read_file(in_fname)
+    numerator = 0
+    for row, out_line in enumerate(out_lines):
+        query = query_lines[row]
+        in_line = in_lines[row]
+        numerator+=query_coverage_recall(query,out_line,in_line)
+    return numerator/(row+1)
+
+
 
 def get_query_include_on_all_output(qfname,out_fname):
     out_lines = read_file(out_fname)
@@ -138,6 +167,7 @@ if __name__=="__main__":
         mfile.write("similarity average to source: "+str(get_semantic_similarity_on_all_output(out_fname,source_fname,segments,model))+"\n")
         mfile.write("average output length: "+str(get_average_length_on_all_output(out_fname,segments))+"\n")
         mfile.write("average coverage ratio:"+str(get_query_coverage_on_all_output(qfname,out_fname))+"\n")
+        mfile.write("average coverage recall ratio:"+str(get_query_coverage_recall_on_all_output(qfname,out_fname,input_fname))+"\n")
         mfile.write("average inclusion ratio:"+str(get_query_include_on_all_output(qfname,out_fname))+"\n")
         mfile.write("average abstractive ratio:"+str(get_abstractive_ratio_on_all_output(input_fname,out_fname,segments))+"\n")
 
