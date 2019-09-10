1ee91f92e8d1fae384c5fd3dd15efc605ab0e0f9
gregory goren <grggoren@gmail.com>
2019-09-10 14:57:34 +0300

    L

--- gen_utils.py
+++ gen_utils.py
@@ -11,6 +11,5 @@ def run_bash_command(command):
     p = subprocess.Popen(command,
                          stdout=subprocess.PIPE,
                          stderr=subprocess.STDOUT, shell=True)
-    # p.wait()
     out, err = p.communicate()
     return out
